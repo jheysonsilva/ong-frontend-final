c2543bddfaec741496b6c8a7f3d869ef6f87b7c7
Jheyson Silva Siqueira <jheyson0295@hotmail.com>
2025-10-17 00:46:28 -0300

    Update contraste.js

--- js/contraste.js
+++ js/contraste.js
@@ -1,8 +1,6 @@
-const btnContraste = document.querySelector('.contraste-btn');
-
-// SOLUÇÃO FINAL: Simplifica a execução para garantir que o evento de clique seja anexado.
-if (btnContraste) { 
-  btnContraste.addEventListener('click', () => {
-    document.body.classList.toggle('alto-contraste');
-  });
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const btnContraste = document.querySelector('.contraste-btn');
+    btnContraste.addEventListener('click', () => {
+        document.body.classList.toggle('alto-contraste');
+    });
+});
