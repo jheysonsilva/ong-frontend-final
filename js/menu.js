679954d25f2e3a5c25b44626b342b3e2d81adb2c
Jheyson Silva Siqueira <jheyson0295@hotmail.com>
2025-10-17 01:12:31 -0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const btnMenu = document.querySelector('.menu-btn');
     const navMenu = document.querySelector('nav ul');
-
     if (btnMenu && navMenu) {
         btnMenu.addEventListener('click', () => {
             navMenu.classList.toggle('mostrar');
         });
-
         btnMenu.addEventListener('keydown', (e) => {
             if (e.key === 'Enter' || e.key === ' ') {
                 e.preventDefault();
