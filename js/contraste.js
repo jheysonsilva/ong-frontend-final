43a53de6e2615eacd1b52260690b3fe5151b1d9a
Jheyson Silva Siqueira <jheyson0295@hotmail.com>
2025-10-16 23:42:08 -0300

    Update contraste.js

--- js/contraste.js
+++ js/contraste.js
@@ -1,5 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', () => {
   const btnContraste = document.querySelector('.contraste-btn');
+  if (!btnContraste) return;
   btnContraste.addEventListener('click', () => {
     document.body.classList.toggle('alto-contraste');
   });
