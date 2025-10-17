7c96dd2a71cab0678246dac1ece226e7bfe74c69
Jheyson Silva Siqueira <jheyson0295@hotmail.com>
2025-10-17 01:21:25 -0300

    Delete js/testeContraste.js

--- js/testeContraste.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const btn = document.querySelector('.contraste-btn');
-    console.log('Botão encontrado?', btn); // 👈 vamos ver se ele acha
-    btn.addEventListener('click', () => {
-        document.body.classList.toggle('alto-contraste');
-        console.log('Botão clicado, contraste ativo?', document.body.classList.contains('alto-contraste'));
-    });
-});
