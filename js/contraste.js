cf22d2b9a95aedfc9260d65c74e179c242f26a96
Jheyson Silva Siqueira <jheyson0295@hotmail.com>
2025-10-16 23:30:40 -0300

    Update contraste.js

--- js/contraste.js
+++ js/contraste.js
@@ -1,12 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const btn = document.querySelector('.contraste-btn');
-
-  if (localStorage.getItem('altoContraste') === 'true') {
-    document.body.classList.add('alto-contraste');
-  }
-
-  btn.addEventListener('click', () => {
+  const btnContraste = document.querySelector('.contraste-btn');
+  btnContraste.addEventListener('click', () => {
     document.body.classList.toggle('alto-contraste');
-    localStorage.setItem('altoContraste', document.body.classList.contains('alto-contraste') ? 'true' : 'false');
   });
 });
